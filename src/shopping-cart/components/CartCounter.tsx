a1c38019072b97c029bb330fc9d3233cb8ad2aa9
wcarbajal <wcarbajal@outlook.com>
2024-01-04 18:22:45 -0500

    tarde del 04/01/2024

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '@/store';
+import { addOne, initCounterState, substractOne } from '@/store/counter/counterSlice';
+import { useEffect } from 'react';
 
 interface Props {
   value?: number;
@@ -10,7 +12,16 @@ interface Props {
 
 export const CartCounter = ({  value = 0 }: Props) => {
 
-  const [count, setCount] = useState(value);
+  
+
+const count = useAppSelector(state => state.counter.count);
+const dispatch = useAppDispatch();
+
+useEffect(() => {
+ dispatch( initCounterState( value));
+
+}, [ dispatch,  value])
+
 
   return (
     <>
@@ -18,13 +29,13 @@ export const CartCounter = ({  value = 0 }: Props) => {
 
       <div className="flex">
         <button
-          onClick={() => setCount(count + 1)}
+          onClick={() => dispatch( addOne() ) }
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
           +1
         </button>
 
         <button
-          onClick={() => setCount(count - 1)}
+          onClick={() => dispatch( substractOne() )}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
           -1
         </button>
--- src/store/counter/counterSlice.ts
+++ src/store/counter/counterSlice.ts
@@ -2,17 +2,26 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 interface CounterState {
   count: number;
+  isReady: boolean;
 }
 
 
 const initialState: CounterState = {
-  count: 5
+  count: 5,
+  isReady: false
+
 }
 
 const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
+    initCounterState (state, action: PayloadAction<number>){
+        if ( state.isReady ) return;
+
+        state.count = action.payload;
+        state.isReady = true;
+    },
     addOne(state){
       state.count++
     },
@@ -21,7 +30,7 @@ const counterSlice = createSlice({
 
       state.count--
     },
-    resetCounter(state, action: PayloadAction<number>){
+    resetCount(state, action: PayloadAction<number>){
       if( action.payload <0 ) action.payload = 0;
       
       state.count = action.payload
@@ -29,6 +38,6 @@ const counterSlice = createSlice({
   }
 });
 
-export const { } = counterSlice.actions;
+export const { initCounterState, addOne, substractOne, resetCount } = counterSlice.actions;
 
 export default counterSlice.reducer;
